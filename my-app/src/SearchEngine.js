d753c002e4f8be3c601a890b10f1139ae8d9ebba
SamCodes3542 <106327181+SamCodes3542@users.noreply.github.com>
2023-02-13 13:03:05 -0600

    installed react remove logo created SearchEngine.js folder

---
+++ my-app/src/SearchEngine.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function SearchEngine() {
+
+    return(
+        <div className="SearchEngine">
+            <div className="card">
+                <div className="card-body>">
+
+            <div className="city">Paris
+            </div>
+            <div className="temperature">
+                87°
+            </div>
+            </div>
+            </div>
+
+</div>
+    );
+    }
